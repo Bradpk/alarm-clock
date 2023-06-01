0a8934dff326fdb50048eeef8e178a03d66bf99c
Brad Kerr <brad.pk@live.com>
2023-06-01 20:18:55 +0000

    g

--- main.js
+++ main.js
@@ -15,21 +15,33 @@ const liveButton = document.getElementById('buttonTrigger')
 
 function currentTime() {
     const time = new Date();
+
     const clockTimeText = time.toLocaleTimeString()
     currentClockTime.innerText = clockTimeText;
 
     const dateText = time.toDateString()
     currentDateTime.innerText = dateText;
 }
-    
+
 //----------------------------------------------------------------
 
 function alarmTracker() {
-const time = new Date();
-const alarmTrack = new Date(time.toDateString() + ' ' + setAlarm.value)
+    const time = new Date();
+    const alarmTrack = new Date(time.toDateString() + '' + setAlarm.value)
+
+    if (time >= alarmTrack) {
+        alert('Wake up!')
+    }
 }
 
 //----------------------------------------------------------------
-    setInterval(currentTime, 1000);
+
+liveButton.addEventListener('click', function () {
+    liveButton.disabled = true
+    setInterval(alarmTracker, 1000)
+})
+
+//----------------------------------------------------------------
+setInterval(currentTime, 1000);
 
 
