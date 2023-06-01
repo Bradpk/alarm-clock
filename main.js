1374f6dc2dbf7b6a1e5ba2ad9a4bcb6c37fc9fc3
Brad Kerr <brad.pk@live.com>
2023-06-01 22:20:14 +0000

    g

--- main.js
+++ main.js
@@ -1,47 +1,39 @@
-// Testing the Javascript file is connected to HTML
-function changeText() {
-    const tryit = document.getElementById("test");
-    tryit.textContent = "Alarm Clock";
-}
-changeText()
-//---------------------------------------------------------------
-
 const currentClockTime = document.getElementById('clockTime');
 const currentDateTime = document.getElementById('dateTime')
-const setAlarm = document.getElementById('alarmInput')
-const liveButton = document.getElementById('buttonTrigger')
+const setAlarm = document.getElementById('alarmInput');
+const liveButton = document.getElementById('buttonTrigger');
+
 
 //----------------------------------------------------------------
+function updateCurrentTime() {
+  const now = new Date();
 
-function currentTime() {
-    const time = new Date();
+  const timeText = now.toLocaleTimeString();
+  currentClockTime.innerText = timeText;
 
-    const clockTimeText = time.toLocaleTimeString()
-    currentClockTime.innerText = clockTimeText;
-
-    const dateText = time.toDateString()
-    currentDateTime.innerText = dateText;
+  const dateText = now.toDateString()
+  currentDateTime.innerText = dateText;
 }
 
 //----------------------------------------------------------------
-
-function alarmTracker() {
-    const time = new Date();
-    const alarmTrack = new Date(time.toDateString() + ' ' + setAlarm.value);
-
-    if (time >= alarmTrack) {
-        alert('Wake up!')
-    }
+function checkAlarm() {
+  const now = new Date();
+  const alarmTrack = new Date(now.toDateString() + ' ' + setAlarm.value);
+  
+  if (now >= alarmTrack) {
+    // Play the alarm sound
+    alert('Alarm!');
+  }
 }
 
 //----------------------------------------------------------------
-
-liveButton.addEventListener('click', function () {
-    liveButton.disabled = true
-    setInterval(alarmTracker, 1000)
-})
+liveButton.addEventListener('click', function() {
+  // Disable the button to prevent multiple alarms
+  liveButton.disabled = true;
+  
+  // Start checking the alarm every second
+  setInterval(checkAlarm, 1000);
+});
 
 //----------------------------------------------------------------
-setInterval(currentTime, 1000);
-
-
+setInterval(updateCurrentTime, 1000);
