7187fdbe4b512575515b2c209315fa32c12c1792
Brad Kerr <brad.pk@live.com>
2023-06-01 15:18:14 +0000

    g

--- main.js
+++ main.js
@@ -10,10 +10,18 @@ function changeText () {
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
+
+    const day = now.getDay();
+    const month = now.getMonth();
+    const year = now.getFullYear();
     
     document.getElementById('hours').textContent = hours
     document.getElementById('minutes').textContent = minutes
     document.getElementById('seconds').textContent = seconds
+        
+    document.getElementById('day').textContent = day
+    document.getElementById('month').textContent = month
+    document.getElementById('year').textContent = year
     }
     setInterval(currentTime, 1000);
     
