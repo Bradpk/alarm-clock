871ea7fdc1238eef45bb1c9b20ec1f5adf35c8a5
Brad Kerr <brad.pk@live.com>
2023-06-01 22:21:42 +0000

    g

--- main.js
+++ main.js
@@ -3,37 +3,40 @@ const currentDateTime = document.getElementById('dateTime')
 const setAlarm = document.getElementById('alarmInput');
 const liveButton = document.getElementById('buttonTrigger');
 
-
 //----------------------------------------------------------------
+
 function updateCurrentTime() {
-  const now = new Date();
+    const now = new Date();
 
-  const timeText = now.toLocaleTimeString();
-  currentClockTime.innerText = timeText;
+    const timeText = now.toLocaleTimeString();
+    currentClockTime.innerText = timeText;
 
-  const dateText = now.toDateString()
-  currentDateTime.innerText = dateText;
+    const dateText = now.toDateString()
+    currentDateTime.innerText = dateText;
 }
 
 //----------------------------------------------------------------
+
 function checkAlarm() {
-  const now = new Date();
-  const alarmTrack = new Date(now.toDateString() + ' ' + setAlarm.value);
-  
-  if (now >= alarmTrack) {
-    // Play the alarm sound
-    alert('Alarm!');
-  }
+    const now = new Date();
+    const alarmTrack = new Date(now.toDateString() + ' ' + setAlarm.value);
+
+    if (now >= alarmTrack) {
+        // Play the alarm sound
+        alert('Alarm!');
+    }
 }
 
 //----------------------------------------------------------------
-liveButton.addEventListener('click', function() {
-  // Disable the button to prevent multiple alarms
-  liveButton.disabled = true;
-  
-  // Start checking the alarm every second
-  setInterval(checkAlarm, 1000);
+
+liveButton.addEventListener('click', function () {
+    // Disable the button to prevent multiple alarms
+    liveButton.disabled = true;
+
+    // Start checking the alarm every second
+    setInterval(checkAlarm, 1000);
 });
 
 //----------------------------------------------------------------
+
 setInterval(updateCurrentTime, 1000);
